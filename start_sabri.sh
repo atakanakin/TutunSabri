d68bc14141e26dd5d0f880a6ccf73dad3f074b7b
Atakan Akın <atakan.akin@commencis.com>
2024-12-11 13:28:07 +0300

    [dev] Update sh

--- start_sabri.sh
+++ start_sabri.sh
@@ -7,7 +7,7 @@ sleep 10
 cd /home/atakan/Documents/Projects/TutunSabri/
 
 # Start tmux session
-tmux new-session -d -s sabri
+tmux new-session -d -s sabri /bin/bash
 
 # Execute main script - this will start the bot - python main.py
 tmux send-keys -t sabri "echo 'Starting...'; python main.py; echo 'Terminated'; sleep 5; echo 'Trying again'; python main.py; sleep 5; sudo reboot" C-m
